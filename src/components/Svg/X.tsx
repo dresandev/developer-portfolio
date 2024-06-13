c3a28360c5e97a6a13c37be0c6144bdb19f18c09
Javier Andres Solarte <79766563+dresandev@users.noreply.github.com>
2024-06-13 01:44:54 -0500

    add: unstyled dialog

---
+++ src/components/Svg/X.tsx
@@ -0,0 +1,20 @@
+import { SVGProps } from "react"
+
+export const X = (props: SVGProps<SVGSVGElement>) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={24}
+		height={24}
+    viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		strokeWidth={2}
+		className="icon icon-tabler icons-tabler-outline icon-tabler-x"
+		{...props}
+	>
+		<path stroke="none" d="M0 0h24v24H0z" />
+		<path d="M18 6 6 18M6 6l12 12" />
+	</svg>
+)
