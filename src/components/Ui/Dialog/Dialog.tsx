15fc42d9650f822c79abeeb1ce4a9b74b015d334
Javier Andres Solarte <79766563+dresandev@users.noreply.github.com>
2024-06-13 20:20:17 -0500

    refactor: add --theme-transition-duration css variable

--- src/components/Ui/Dialog/Dialog.tsx
+++ src/components/Ui/Dialog/Dialog.tsx
@@ -12,7 +12,7 @@ export const DialogClose = DialogPrimitive.Close
 export const DialogOverlay = forwardRef<
 	React.ElementRef<typeof DialogPrimitive.Overlay>,
 	React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
->(({ className, ...props }, ref) => <DialogPrimitive.Overlay ref={ref} className={""} {...props} />)
+>(({ className, ...props }, ref) => <DialogPrimitive.Overlay ref={ref} className="" {...props} />)
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 
 export const DialogContent = forwardRef<
@@ -21,12 +21,9 @@ export const DialogContent = forwardRef<
 >(({ className, children, ...props }, ref) => (
 	<DialogPortal>
 		<DialogOverlay />
-		<DialogPrimitive.Content ref={ref} className={""} {...props}>
+		<DialogPrimitive.Content ref={ref} className="" {...props}>
 			{children}
-			<DialogClose
-				aria-label="Cerrar"
-				className="absolute right-[10px] top-[10px] text-[hsl(0_0%_90%/.5)] transition-[color] hover:text-text"
-			>
+			<DialogClose aria-label="Cerrar" className="">
 				<X className="size-4" />
 			</DialogClose>
 		</DialogPrimitive.Content>
@@ -37,5 +34,5 @@ DialogContent.displayName = DialogPrimitive.Content.displayName
 export const DialogTitle = forwardRef<
 	React.ElementRef<typeof DialogPrimitive.Title>,
 	React.ComponentPropsWithoutRef<typeof DialogPrimitive.Title>
->(({ className, ...props }, ref) => <DialogPrimitive.Title ref={ref} className={""} {...props} />)
+>(({ className, ...props }, ref) => <DialogPrimitive.Title ref={ref} className="" {...props} />)
 DialogTitle.displayName = DialogPrimitive.Title.displayName
