43077189b94cc7c644e605208235d80f2f78369a
Javier Andres Solarte <79766563+dresandev@users.noreply.github.com>
2024-07-24 01:22:10 -0500

    style: improve about text

--- src/app/(with-footer)/(home)/_sections/AboutSection/AboutSection.tsx
+++ src/app/(with-footer)/(home)/_sections/AboutSection/AboutSection.tsx
@@ -5,19 +5,20 @@ export const AboutSection = () => {
 		<section className="section-wrapper">
 			<h2 className={styles.title}>Un poco sobre mí</h2>
 			<div className={styles.aboutMeWrapper}>
-				<p>Hola, hola 👋🏻, me llamo Javier Andrés, pero puedes decirme Dresan.</p>
+				<p>Hola 👋🏻, me llamo Javier Andrés, pero puedes decirme Dresan.</p>
 				<p>
 					Soy desarrollador de software y me especializo en el Front-end. Valoro los pequeños
 					detalles y soy un gran fan de CSS (¡el mejor lenguaje de programación! 🙂).
 				</p>
 				<p>
-					Disfruto aprendiendo sobre el desarrollo de software en sus distintas etapas y
-					manteniéndome al tanto de los avances en inteligencia artificial 🥴.
+					Me gusta aprender sobre desarrollo de software en sus distintas etapas y manteniéndome al
+					tanto de los avances en inteligencia artificial 🥴.
 				</p>
 				<p>
 					Actualmente, estoy profundizando mi conocimiento en el ecosistema de React, pero siempre
-					estoy dispuesto a aprender nuevas tecnologías 😉.
+					estoy dispuesto a aprender (Vue.js) nuevas tecnologías 😉.
 				</p>
+				<p>Gran fan de la naturaleza y el senderismo 🏞️.</p>
 			</div>
 		</section>
 	)
